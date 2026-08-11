7c55b8f1205289c8cba0f8f8212d6b39beb90ee1
Abdul Musawir <abdulmusawir8191456@gmail.com>
2026-08-11 20:14:26 +0500

    Update add_node_k8.sh

--- add_node_k8.sh
+++ add_node_k8.sh
@@ -55,5 +55,3 @@ swapoff -a
 ## Paste the contents on worker node and enter 
 ## Post sucessful execution; Check cluster status on master node command $kubectl get nodes
 ## finisH
-
-## Thanks to my good friend Giri: https://github.com/learnbyseven
